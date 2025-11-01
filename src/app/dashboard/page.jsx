f629491baeb416bae50a8ac587989be535376aef
Marcelo Sanchez <marcedgardosanchez@gmail.com>
2025-11-01 13:41:00 -0300

    mejoramos la IU del dashboard

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -7,36 +7,6 @@ import MainComponent from "../components/mainComponent";
 export default function DashboardPage() {
   return (
     <MainComponent>
-      {/* Grid responsivo: 1 col en mobile, 3 en desktop 
-      <h1 className="text-4xl md:text-6xl font-bold max-w-4xl leading-tight my-4">
-        Dashboard
-      </h1>
-      <p className="text-lg md:text-2xl text-gray-600 max-w-xl leading-relaxed mb-6">
-        Bienvenido, estás autenticado.
-      </p>
-      <div className="grid grid-cols-1 lg:grid-cols-3 grid-rows-2 gap-4 p-5">
-        
-        <div className="md:col-span-2 row-span-2 ">
-          
-          <div className="w-full ">
-            
-          </div>
-        </div>
-       
-        <div className="lg:row-span-1 rounded-2xl border  p-4 h-full">
-          <div className="h-full">
-            
-          </div>
-        </div>
-
-        
-        <div className="lg:col-span-1 rounded-2xl border  p-4 h-full">
-          <div className="h-full">
-            
-          </div>
-        </div>
-      </div>
-      */}
       <h1 className="text-4xl md:text-6xl font-bold max-w-4xl leading-tight my-4">
         Dashboard
       </h1>
